b475017a7aece6b695b6daac1696010b3b63af2f
Saulo Arruda <sauloarruda@gmail.com>
2010-10-31 18:14:24 -0300

    fix for hibernate validation issue, current issue: problem with html/xml entities in pt-br

--- src/main/java/br/com/jera/example/domain/Contact.java
+++ src/main/java/br/com/jera/example/domain/Contact.java
@@ -6,10 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import org.hibernate.validator.NotEmpty;
+import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
 
 @Entity
 @Name("contact")
+@Scope(ScopeType.EVENT)
 public class Contact implements Serializable {
 
 	private static final long serialVersionUID = 7067862717942009603L;
@@ -18,6 +22,7 @@ public class Contact implements Serializable {
 	@GeneratedValue
 	private Integer id;
 
+	@NotEmpty
 	private String lastName;
 
 	public Contact() {
--- src/main/java/br/com/jera/example/jsf/EditContactController.java
+++ src/main/java/br/com/jera/example/jsf/EditContactController.java
@@ -1,12 +1,15 @@
 package br.com.jera.example.jsf;
 
+import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
 
 import br.com.jera.example.domain.AddressBook;
 import br.com.jera.example.domain.Contact;
 
 @Name("editContactController")
+@Scope(ScopeType.EVENT)
 public class EditContactController {
 
 	@In(create = true)
