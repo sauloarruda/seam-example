ef02f3238a7a7f2b8e776ea4c34532a2fe8528ed
Saulo Arruda <sauloarruda@gmail.com>
2010-10-31 21:17:47 -0300

    testing for required lastName field for Contact

--- src/test/java/br/com/jera/example/domain/AddressBookTest.java
+++ src/test/java/br/com/jera/example/domain/AddressBookTest.java
@@ -3,6 +3,8 @@ package br.com.jera.example.domain;
 import static org.hamcrest.Matchers.hasItem;
 import static org.junit.Assert.assertThat;
 
+import javax.persistence.PersistenceException;
+
 import org.junit.Test;
 
 import br.com.jera.example.test.DomainObjectTestCase;
@@ -10,7 +12,7 @@ import br.com.jera.example.test.DomainObjectTestCase;
 public class AddressBookTest extends DomainObjectTestCase {
 
 	@Test
-	public void createNewContactWithMinimalInformation() throws Exception {
+	public void successCreatingNewContactWithMinimalInformation() throws Exception {
 		new ComponentTest() {
 			@Override
 			protected void testComponents() throws Exception {
@@ -19,6 +21,19 @@ public class AddressBookTest extends DomainObjectTestCase {
 				Contact contact = new Contact("Arruda");
 				addressBook.add(contact);
 				assertThat(addressBook.search(contact.getLastName()), hasItem(contact));
+			}
+		}.run();
+	}
+
+	@Test(expected = PersistenceException.class)
+	public void failCreatingContactWithoutRequiredInformation() throws Exception {
+		new ComponentTest() {
+			@Override
+			protected void testComponents() throws Exception {
+				AddressBook addressBook = (AddressBook) getInstance("addressBook");
+
+				Contact contact = new Contact();
+				addressBook.add(contact);
 			}
 		}.run();
 	}
